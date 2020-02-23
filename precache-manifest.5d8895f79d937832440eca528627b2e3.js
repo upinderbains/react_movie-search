a641c8f78c91c798c698b96b79ce9e7b857c7fda
Upinder Bains <bains@Upinders-MacBook-Pro.local>
2020-02-23 15:04:38 -0500

    Updates

---
+++ precache-manifest.5d8895f79d937832440eca528627b2e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fed5bf387135fe8b79b1316f43ca26c",
+    "url": "/react_movie-search/index.html"
+  },
+  {
+    "revision": "67114cf4386f388c3dda",
+    "url": "/react_movie-search/static/js/2.558950cf.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react_movie-search/static/js/2.558950cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41efc1b9b8309f9c3ddb",
+    "url": "/react_movie-search/static/js/main.eacdc28c.chunk.js"
+  },
+  {
+    "revision": "9440c627d852d545c5b3",
+    "url": "/react_movie-search/static/js/runtime-main.48982511.js"
+  }
+]);
